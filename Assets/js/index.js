d523bb8a181bd1ff48dd3d0e2acb51c0562f6df9
DerekHan <donglak.han@gmail.com>
2023-10-05 14:33:27 -0500

    connet two vidoe

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,26 +1,35 @@
+// Declaring variables for local and remote streams, usernames, and the peerconnection
 let localStream;
 let username;
 let remoteUser;
 let remoteStream;
 
+// Parsing the current URL to get the "username" and "remoteUser" from the URL parameters.
 let url = new URL(window.location.href);
 username = url.searchParams.get("username");
-remoteUser = url.searchParams.get("remoteUser");
+remoteUser = url.searchParams.get("remoteuser");
 
 let peerConnection;
+
+// Initializing the video and audio stream and creating the offer.
 let init = async () => {
   localStream = await navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true,
   });
+  // Set the local video element's source to the obtained local stream.
   document.getElementById("user-1").srcObject = localStream;
+
+  // Initiate the process to create the webRTC offer.
   createOffer();
 };
 
 init();
 
+// Establishing a connection to the socket.io server
 let socket = io.connect();
 
+// Emitting a 'userconnect' event upon successfully connectiong to the Socket.io server.
 socket.on("connect", () => {
   if (socket.connected) {
     socket.emit("userconnect", {
@@ -29,6 +38,7 @@ socket.on("connect", () => {
   }
 });
 
+// STUN servers configuration for webRTC
 let servers = {
   iceServers: [
     {
@@ -37,43 +47,54 @@ let servers = {
   ],
 };
 
+// Creating the RTCPeerConnection and setting up the required event handlers.
 let createPeerConnection = async () => {
+  // Creating a new peer connection
   peerConnection = new RTCPeerConnection(servers);
 
+  // Setting up a new media stream for the remote user.
   remoteStream = new MediaStream();
 
+  // Setting the video element's source for the remote user.
   document.getElementById("user-2").srcObject = remoteStream;
 
-  localStream.getTrack().foreach((track) => {
+  // Adding tracks from the local stream to the peer connection.
+  localStream.getTracks().forEach((track) => {
     peerConnection.addTrack(track, localStream);
   });
 
+  // Handling the 'ontrack' event when remote tracks are received.
   peerConnection.ontrack = async (event) => {
-    event.stream[0].getTracks().foreach((track) => {
+    event.streams[0].getTracks().forEach((track) => {
       remoteStream.addTrack(track);
     });
   };
 
+  // Handling the inactivity of the remote stream.
+  // Disabling all tracks and closing the connection when the remote stream becomes inactive.
   remoteStream.oninactive = () => {
-    remoteStream.getTracks().foreach((track) => {
+    remoteStream.getTracks().forEach((track) => {
       track.enabled = !track.enabled;
     });
     peerConnection.close();
   };
 
-  peerConnection.remoteStream = peerConnection.onicecandidate = async (
-    event
-  ) => {
-    if (event.candidate) {
-      socket.emit("candidateSentToUser", {
-        username: username,
-        remoteUser: remoteUser,
-        iceCandidateData: event.candidate,
-      });
-    }
-  };
+  // peerConnection.remoteStream = peerConnection.onicecandidate = async (
+  //   event
+  // ) => {
+  //   if (event.candidate) {
+  //     socket.emit("candidateSentToUser", {
+  //       username: username,
+  //       remoteUser: remoteUser,
+  //       iceCandidateData: event.candidate,
+  //     });
+  //   }
+  // };
 
+  // Handling the 'onicecandidate' event. Emitting the candidate to the other peer via the server.
+  // Note: This appears twice and seems redundant. You probably need only one.
   peerConnection.onicecandidate = async (event) => {
+    console.log("Local ICE Candidate:", event.candidate);
     if (event.candidate) {
       socket.emit("candidateSentToUser", {
         username: username,
@@ -84,6 +105,7 @@ let createPeerConnection = async () => {
   };
 };
 
+// Creating and sending an offer to the remote user.
 let createOffer = async () => {
   createPeerConnection();
   let offer = await peerConnection.createOffer();
@@ -95,14 +117,13 @@ let createOffer = async () => {
   });
 };
 
+// Creating an answer in response to an offer received from the remote user.
 let createAnswer = async (data) => {
   remoteUser = data.remoteUser;
-
   createPeerConnection();
-
   await peerConnection.setRemoteDescription(data.offer);
   let answer = await peerConnection.createAnswer();
-
+  await peerConnection.setLocalDescription(answer);
   socket.emit("answerSentToUser1", {
     answer: answer,
     sender: data.remoteUser,
@@ -110,20 +131,24 @@ let createAnswer = async (data) => {
   });
 };
 
-socket.on("ReceiverOffer", function (data) {
+// Handling the 'ReceiveOffer' event. Creating an answer when an offer is received.
+socket.on("ReceiveOffer", function (data) {
   createAnswer(data);
 });
 
+// Setting the remote description when an answer is received.
 let addAnswer = async (data) => {
   if (!peerConnection.currentRemoteDescription) {
     peerConnection.setRemoteDescription(data.answer);
   }
 };
 
-socket.on("ReceiverAnswer", function (data) {
+// Handling the 'ReceiveAnswer' event.
+socket.on("ReceiveAnswer", function (data) {
   addAnswer(data);
 });
 
+// Adding the received ice candidate to the peer connection.
 socket.on("candidateReceiver", function (data) {
   peerConnection.addIceCandidate(data.iceCandidateData);
 });
--- server.js
+++ server.js
@@ -1,34 +1,49 @@
+// Importing neccessary libraries/modules
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+
+// Setting up the port, using the environment variable PORT if it's available, else 8080
 const PORT = process.env.PORT || 8080;
+
+// Initializing the express application
 const app = express();
 
+// Middleware to parse incoming request bodies with urlencoded payloads
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Middleware to parse incoming request bodies with a JSON payload
 app.use(bodyParser.json());
 
+// Setting up EJS (Embedded JavaScript) as the view engine for the Express application
 app.set("view engine", "ejs");
 
+// Serving static assets like CSS, images. and JS from the Assets directory
 app.use("/css", express.static(path.resolve(__dirname, "Assets/css")));
 app.use("/img", express.static(path.resolve(__dirname, "Assets/img")));
 app.use("/js", express.static(path.resolve(__dirname, "Assets/js")));
 
+// Using; the router middleware from the router.js file for the root path
 app.use("/", require("./Server/routes/router.js"));
 
+// Starting the Express server and listening on the specified port
 var server = app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
+// Initiating Socket.io on the running server
 const io = require("socket.io")(server, {
-  allowEI03: true, // False by default
+  allowEI03: true, // This allows older Socket.io v3 clients to connect to the server
 });
 
+// An array to hold details of users connected to the server
 var userConnection = [];
 
+// Event listner for when a new client connects to the Socket.io server
 io.on("connection", (socket) => {
   console.log("Socket id: ", socket.id);
 
+  // Event listener for a custom event named "userconnect"
   socket.on("userconnect", (data) => {
     userConnection.push({
       connectionId: socket.id,
@@ -39,16 +54,18 @@ io.on("connection", (socket) => {
     console.log("userCount", userCount);
   });
 
+  // Event listener for a custom event when a user sends an offer to a remote user
   socket.on("offerSentToRemote", (data) => {
     var offerReceiver = userConnection.find(
       (o) => o.user_id === data.remoteUser
     );
     if (offerReceiver) {
       console.log("OfferReceiver user is:", offerReceiver.connectionId);
-      socket.to(offerReceiver.connectionId).emit("ReceiverOffer", data);
+      socket.to(offerReceiver.connectionId).emit("ReceiveOffer", data);
     }
   });
 
+  // Event listener for when an answer is sent to the initiator of the connection
   socket.on("answerSentToUser1", (data) => {
     var answerReceiver = userConnection.find(
       (o) => o.user_id === data.receiver
@@ -60,14 +77,37 @@ io.on("connection", (socket) => {
     }
   });
 
+  // Event listener for when a candidate information (ICE Candidate) is sent to another user
   socket.on("candidateSentToUser", (data) => {
-    var candidateReceiver = userConnection.find(
-      (o) => o.user_id === data.receiver
-    );
+    // console.log(
+    //   "Received candidate from:",
+    //   data.username,
+    //   "for:",
+    //   data.remoteUser
+    // );
 
+    var candidateReceiver = userConnection.find(
+      (o) => o.user_id === data.remoteUser
+    );
+    // console.log("candidateReceiver", candidateReceiver);
     if (candidateReceiver) {
-      console.log("CandidateReceiver user is:", candidateReceiver.connectionId);
-      socket.to(candidateReceiver.connectionId).emit("CandidateReceiver", data);
+      console.log("candidateReceiver user is:", candidateReceiver.connectionId);
+      socket.to(candidateReceiver.connectionId).emit("candidateReceiver", data);
+    }
+  });
+
+  // Event listener for when a user disconnects from the Socket.io server
+  socket.on("disconnect", () => {
+    console.log("User disconnected from server");
+    var disUser = userConnection.find((p) => p.connectionId === socket.id);
+    if (disUser) {
+      userConnection = userConnection.filter(
+        (p) => p.connectionId !== socket.id
+      );
+      console.log(
+        "Rest users username are: ",
+        userConnection.map((user) => user.user_id)
+      );
     }
   });
 });
